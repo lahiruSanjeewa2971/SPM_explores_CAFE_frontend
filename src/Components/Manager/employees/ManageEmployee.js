5d15a872cd2c5d5b289d59a900b220686bfeea2c
Lahiru Sanjeewa <it19166834@my.sliit.lk>
2021-08-17 12:28:09 +0530

    finished side nav (basic things)

---
+++ src/Components/Manager/employees/ManageEmployee.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ManageEmployee(){
+    return (
+        <div className="report" style={{paddingLeft:"310px"}}>
+            <h1>Manage Employee</h1>
+        </div>
+    )
+}
